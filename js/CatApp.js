d0c6bae564651d59df13b576bbb6c129bd4d2657
LAUREN CORFIELD <b4017553@hallam.shu.ac.uk>
2021-01-12 12:54:10 +0000

    Changes to Homepage - trying to add topbar with search bar

--- js/CatApp.js
+++ js/CatApp.js
@@ -1,10 +1,4 @@
 var http = require('http');
-      
-
-          
-    
-
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
