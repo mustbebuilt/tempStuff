6c492b77184f31167619aa64cfcbc84ea2881c3d
JasperShaw <b9014224@my.shu.ac.uk>
2021-01-13 11:12:58 +0000

    Push to sort issue

--- js/main.js
+++ js/main.js
@@ -20,23 +20,20 @@ function navHighlight(elem, home, active) {
     }
 navHighlight('.mainNav ul li a', 'index.html', 'current'); /* menu link selector, home page, highlight class */
 })();
-function openTab(evt, tabName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
